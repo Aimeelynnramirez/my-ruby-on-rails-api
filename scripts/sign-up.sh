d0457d8063f115a74875d039a520d10b1679f022
Aimee Lynn <aimeelynnramirez@gmail.com>
2019-05-16 03:50:56 -0400

    make it better

---
+++ scripts/sign-up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+EMAIL=ava@bob.com 
+PASSWORD=hannah 
+API="${API_ORIGIN:-http://localhost:3000}"
+URL_PATH="/sign-up"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
